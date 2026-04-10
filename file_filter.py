abe3ea4b86af95625e405d3fc8e8899a992c69a8
wayttyyyy <dimagoldinskij@gmail.com>
2026-04-10 14:57:29 +0300

    main program for manual usage

--- file_filter.py
+++ file_filter.py
@@ -11,3 +11,17 @@ def save_lines_to_file(file_path: str, lines: list):
     """Записує список рядків у вказаний файл."""
     with open(file_path, 'w', encoding='utf-8') as file:
         file.writelines(lines)
+
+
+if __name__ == "__main__":
+    source_file = "source.txt"
+    word_to_find = "Apple"
+    output_file = "filtered.txt"
+
+    found_lines = get_filtered_lines(source_file, word_to_find)
+    save_lines_to_file(output_file, found_lines)
+    
+    print(f"Готово! Программа знайшла рядків: {len(found_lines)}.")
+    print("Ось вони:")
+    for line in found_lines:
+        print(f" - {line.strip()}")
